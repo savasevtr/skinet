3e3c6da64d2c8d824f85b5b55a164216b7b51b54
savasevtr <savas.ev@yahoo.com>
2021-01-31 22:56:07 +0300

    Client - Identity - 1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mode => mode.ShopModule), data: { breadcrumb: 'Shop' } },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mode => mode.BasketModule), data: { breadcrumb: 'Basket' } },
   { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mode => mode.CheckoutModule), data: { breadcrumb: 'Checkout' } },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(mode => mode.AccountModule), data: { breadcrumb: { skip: true } } },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
